67e662e65ae88302c8ae71b774ad8d94713b014f
gth1ago <gabriel.thiagohs@gmail.com>
2023-02-08 00:16:51 -0300

    importa dataset

--- IA2/importaDados.py
+++ IA2/importaDados.py
@@ -1,15 +1,13 @@
-
-
-import sqlite3
+import os
 import pandas as pd
 
-#data_paths = [os.path.join(pth, f)
-#for pth, dirs, files in os.walk('../input/soccer/') for f in files]
+# caminho para dataset (windows/linux)
+data_path = os.path.join(os.path.dirname(__file__), "dataset", "brasileirao.csv")
 
-#(after executing the above two code statements we get path directory - '../input/soccer/database.sqlite')
+# Importa base de dados
+df = pd.read_csv(data_path)
 
-#After we got our directory path we have to import the data for that we can use the below code.
+print(df)
 
-
-cnx = sqlite3.connect('database.sqlite')
-df = pd.read_sql_query("SELECT * FROM Player", cnx)
+totalLinha = len(df)
+print(totalLinha)

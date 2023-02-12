3cc5ffd64ba116fa67ce4b3baba121c0c4316451
Sersasj <sasjsergioalvarezjunior@gmail.com>
2023-02-11 21:47:23 -0300

    função utilidade??

--- IA2/TrabalhoFifa.py
+++ IA2/TrabalhoFifa.py
@@ -3,7 +3,7 @@ from natsort import natsort_keygen
 import csv
 from unidecode import unidecode
 import unicodedata
-
+import numpy as np
 # Carrega dados dos anos 2019, 2020 e 2021
 df_brasileirao = pd.read_csv('dataset/brasileirao.csv')
 df_brasileirao = df_brasileirao[df_brasileirao['year'].isin([2019,2020,2021])]
@@ -50,21 +50,76 @@ mean_table = merged_df.groupby(["category"]).mean(numeric_only='True')
 
 ##########
 
-def calculate_utility(mean_table, entry_table):
-    print("aa")
-    #(overall_G4 - overall_time) / overall_G4
+def calculate_utility(club_name, mean_table, team_table):
 
+    # Define the probability tables for each variable
+    overall_probs = np.array(mean_table['overall'].tolist())/100
+    pace_probs = np.array(mean_table['pace'].tolist())/100
+    shooting_probs = np.array(mean_table['shooting'].tolist())/100
+    passing_probs = np.array(mean_table['passing'].tolist())/100
+    defending_probs = np.array(mean_table['defending'].tolist())/100
+    
+    overall_team = np.array(team_table['overall'].tolist())/100
+    pace_team = np.array(team_table['pace'].tolist())/100
+    shooting_team = np.array(team_table['shooting'].tolist())/100
+    passing_team = np.array(team_table['passing'].tolist())/100
+    defending_team = np.array(team_table['defending'].tolist())/100 
+    
 
+    probs = np.zeros(3)
+    for i in range(3):
+        overall = 1 - abs((overall_probs[i] - overall_team)) / overall_probs[i] 
+        pace = 1 - abs((pace_probs[i] - pace_team))/ pace_probs[i] 
+        shooting = 1 - abs((shooting_probs[i] - shooting_team)) / shooting_probs[i] 
+        passing = 1 - ((passing_probs[i] - passing_team)) / passing_probs[i] 
+        defending = 1 - abs((defending_probs[i] - defending_team)) / defending_probs[i] 
+        probs[i] = overall + pace + shooting + passing + defending
+        
+        
+    team_position = np.argmax(probs)
+
+    if team_position == 0:        
+        print("O {} terminará o campeonato no G4".format(club_name))
+    elif team_position == 1:        
+        print("O {} terminará o campeonato neutro".format(club_name))
+    else:
+        print("O {} terminará o campeonato na zona de rebaixamento".format(club_name))
+        
+    print("Função utilidade: ",probs)  
 ##############
-# Simulação
+# Simulação palmeiras
 df_brasileirao_sim = pd.read_csv('dataset/brasileirao.csv')
 df_brasileirao_sim = df_brasileirao_sim[df_brasileirao_sim['year'] == 2022]
 df_brasileirao_sim.rename(columns={'team': 'club_name'}, inplace=True)
 
 simulation_df = pd.read_csv("dataset_fifa/players_22.csv", low_memory=False)
-df_players_palmeiras = simulation_df[simulation_df["club_name"] == 'Palmeiras']
-df_players_palmeiras = df_players_palmeiras[['club_name', 'overall', 'age', 'pace', 'shooting', 'passing', 'defending']]
-df_players_palmeiras.fillna(df_players_palmeiras.mean(numeric_only=True), inplace=True)
-df_players_palmeiras_mean = df_players_palmeiras.groupby(["club_name"]).mean(numeric_only='True')
+df_players_sim = simulation_df[simulation_df["club_name"] == 'Palmeiras']
+df_players_sim = df_players_sim[['club_name', 'overall', 'age', 'pace', 'shooting', 'passing', 'defending']]
+df_players_sim.fillna(df_players_sim.mean(numeric_only=True), inplace=True)
+df_players_sim_mean = df_players_sim.groupby(["club_name"]).mean(numeric_only='True')
+calculate_utility('Palmeiras', mean_table, df_players_sim_mean)
 
-calculate_utility(mean_table, df_players_palmeiras_mean)
+
+# Simulação Botafogo
+df_brasileirao_sim = pd.read_csv('dataset/brasileirao.csv')
+df_brasileirao_sim = df_brasileirao_sim[df_brasileirao_sim['year'] == 2022]
+df_brasileirao_sim.rename(columns={'team': 'club_name'}, inplace=True)
+
+simulation_df = pd.read_csv("dataset_fifa/players_22.csv", low_memory=False)
+df_players_sim = simulation_df[simulation_df["club_name"] == 'Santos']
+df_players_sim = df_players_sim[['club_name', 'overall', 'age', 'pace', 'shooting', 'passing', 'defending']]
+df_players_sim.fillna(df_players_sim.mean(numeric_only=True), inplace=True)
+df_players_sim_mean = df_players_sim.groupby(["club_name"]).mean(numeric_only='True')
+calculate_utility('Goiás', mean_table, df_players_sim_mean)
+
+# Simulação Atlético Clube Goianiense
+df_brasileirao_sim = pd.read_csv('dataset/brasileirao.csv')
+df_brasileirao_sim = df_brasileirao_sim[df_brasileirao_sim['year'] == 2022]
+df_brasileirao_sim.rename(columns={'team': 'club_name'}, inplace=True)
+
+simulation_df = pd.read_csv("dataset_fifa/players_22.csv", low_memory=False)
+df_players_sim = simulation_df[simulation_df["club_name"] == 'Atlético Clube Goianiense']
+df_players_sim = df_players_sim[['club_name', 'overall', 'age', 'pace', 'shooting', 'passing', 'defending']]
+df_players_sim.fillna(df_players_sim.mean(numeric_only=True), inplace=True)
+df_players_sim_mean = df_players_sim.groupby(["club_name"]).mean(numeric_only='True')
+calculate_utility('Atlético Clube Goianiense', mean_table, df_players_sim_mean)

6cfb52b09f49561175d37c0798bc001f702c8469
Sersasj <sasjsergioalvarezjunior@gmail.com>
2023-02-11 22:52:15 -0300

    alteraçoes

--- IA2/TrabalhoFifa.py
+++ IA2/TrabalhoFifa.py
@@ -29,6 +29,7 @@ df_players = df_players[df_players["club_name"].isin(times_brasileirao)]
 # Pega infos que queremos
 df_players = df_players[['club_name', 'overall', 'age', 'pace', 'shooting', 'passing', 'defending']]
 # Substitui nan pela média
+
 df_players.fillna(df_players.mean(numeric_only=True), inplace=True)
 
 
@@ -50,9 +51,8 @@ mean_table = merged_df.groupby(["category"]).mean(numeric_only='True')
 
 ##########
 
-def calculate_utility(club_name, mean_table, team_table):
-
-    # Define the probability tables for each variable
+def calculate_utility(club_name, mean_table, team_table):   
+    
     overall_probs = np.array(mean_table['overall'].tolist())/100
     pace_probs = np.array(mean_table['pace'].tolist())/100
     shooting_probs = np.array(mean_table['shooting'].tolist())/100
@@ -68,11 +68,11 @@ def calculate_utility(club_name, mean_table, team_table):
 
     probs = np.zeros(3)
     for i in range(3):
-        overall = 1 - abs((overall_probs[i] - overall_team)) / overall_probs[i] 
-        pace = 1 - abs((pace_probs[i] - pace_team))/ pace_probs[i] 
-        shooting = 1 - abs((shooting_probs[i] - shooting_team)) / shooting_probs[i] 
-        passing = 1 - ((passing_probs[i] - passing_team)) / passing_probs[i] 
-        defending = 1 - abs((defending_probs[i] - defending_team)) / defending_probs[i] 
+        overall = (1 - abs((overall_probs[i] - overall_team)) / overall_probs[i]) * 0.3 
+        pace = (1 - abs((pace_probs[i] - pace_team))/ pace_probs[i]) * 0.1
+        shooting = (1 - abs((shooting_probs[i] - shooting_team)) / shooting_probs[i]) * 0.35 
+        passing = (1 - ((passing_probs[i] - passing_team)) / passing_probs[i]) * 0.1
+        defending = (1 - abs((defending_probs[i] - defending_team)) / defending_probs[i]) * 0.15 
         probs[i] = overall + pace + shooting + passing + defending
         
         
@@ -85,7 +85,10 @@ def calculate_utility(club_name, mean_table, team_table):
     else:
         print("O {} terminará o campeonato na zona de rebaixamento".format(club_name))
         
-    print("Função utilidade: ",probs)  
+    print("Função utilidade: ",probs) 
+
+
+
 ##############
 # Simulação palmeiras
 df_brasileirao_sim = pd.read_csv('dataset/brasileirao.csv')
@@ -99,8 +102,7 @@ df_players_sim.fillna(df_players_sim.mean(numeric_only=True), inplace=True)
 df_players_sim_mean = df_players_sim.groupby(["club_name"]).mean(numeric_only='True')
 calculate_utility('Palmeiras', mean_table, df_players_sim_mean)
 
-
-# Simulação Botafogo
+# Simulação Santos
 df_brasileirao_sim = pd.read_csv('dataset/brasileirao.csv')
 df_brasileirao_sim = df_brasileirao_sim[df_brasileirao_sim['year'] == 2022]
 df_brasileirao_sim.rename(columns={'team': 'club_name'}, inplace=True)
@@ -110,7 +112,7 @@ df_players_sim = simulation_df[simulation_df["club_name"] == 'Santos']
 df_players_sim = df_players_sim[['club_name', 'overall', 'age', 'pace', 'shooting', 'passing', 'defending']]
 df_players_sim.fillna(df_players_sim.mean(numeric_only=True), inplace=True)
 df_players_sim_mean = df_players_sim.groupby(["club_name"]).mean(numeric_only='True')
-calculate_utility('Goiás', mean_table, df_players_sim_mean)
+calculate_utility('Santos', mean_table, df_players_sim_mean)
 
 # Simulação Atlético Clube Goianiense
 df_brasileirao_sim = pd.read_csv('dataset/brasileirao.csv')

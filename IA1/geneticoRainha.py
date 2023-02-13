68a25fbb4ae6d414a2f48d25cc1c592247794251
Sersasj <sasjsergioalvarezjunior@gmail.com>
2023-02-12 22:21:04 -0300

    pdf

--- IA1/geneticoRainha.py
+++ IA1/geneticoRainha.py
@@ -1,187 +1,283 @@
-from code import interact
-from typing import List, Tuple
+import time
 import random
-from statistics import mean, stdev
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
 
-# fitness = numero de pares de rainhas (i, j), i < j que não se atacam
-# assim, o maximo de fitness é um somatório de 1 até (numRainhas - 1)
-def calculoFitness(rainhas: List[int]) -> int:
-    n = len(rainhas)
-    fitness: int = 0
-    for i in range(n):
-        for j in range(i+1, n):
-            if rainhas[i] == rainhas[j]:
+def create_chromosome(number_of_queens):
+    """
+    Cria um cromossomo aleatório com as possíveis posições das damas
+    não existe posição repetida
+    Parâmetros:
+    number_of_queens -- quantidade de damas no tabuleiro 
+    Retorna:
+    np.array de tamanho [1, ..., number_of_queens] 
+    """
+    chromosome = np.arange(number_of_queens)
+    np.random.shuffle(chromosome)
+    return chromosome    
+    
+def fitness(chromosome):
+    """
+    Calcula o fitness do cromossomo
+    o fitness é calculado pelo numero de pares de rainhas (i, j), i < j que não se atacam
+    o maximo de fitness é um somatório de 1 até (numRainhas - 1)
+    ex: em um tabuleiro 8X8 o fitness maximo é 28
+    Parâmetros:
+    chromosome -- np.array com a posição de cada rainha no tabuleiro
+    Retorna:
+    inteiro com a fitness do cromossomo.
+    """
+    fitness = 0
+    for i in range(len(chromosome)):
+        for j in range(i+1, len(chromosome)):
+            # horizontal
+            if chromosome[i] == chromosome[j]:
                 continue
-            if j-i == abs(rainhas[i] - rainhas[j]):
+            # diagonal
+            if j-i == abs(chromosome[i] - chromosome[j]):
                 continue
             fitness += 1
-    return fitness
+    return fitness    
 
-# retorna uma tupla com a melhor solucao e o fitness da melhor solucao da populacao
-def selecionaMelhorSolucao(populacao: List[List[int]], fitPopulacao: List[int]) -> Tuple[List[int], int]:
-    idxMelhorSolucao: int = 0
-    melhorFitness: int = fitPopulacao[0]
-    for i in range(1, len(fitPopulacao)):
-        if fitPopulacao[i] > melhorFitness:
-            idxMelhorSolucao = i
-            melhorFitness = fitPopulacao[i]
-    return tuple((populacao[idxMelhorSolucao], melhorFitness))
 
-# retorna uma lista com o indice do pior fitness da populacao
-# caso haja empate, o elemento empatado também é adicionado à lista
-def selecionaIdxPiorFitness(fitPopulacao: List[int]) -> List[int]:
-    idxPiorFitness: List[int] = [0]
-    piorFitness: int = fitPopulacao[0]
-    for i in range(1, len(fitPopulacao)):
-        if fitPopulacao[i] < piorFitness:
-            idxPiorFitness = [i]
-            piorFitness = fitPopulacao[i]
-        elif fitPopulacao[i] == piorFitness:
-            idxPiorFitness.append(i)
-    return idxPiorFitness
+def mutate(chromosome):
+    """
+    Muta um cromossomo
+    a mutação troca a posição de duas damas no cromossomo 
+    Parâmetros:
+    chromosome -- np.array com a posição de cada rainha no tabuleiro
+    Retorna:
+    cromossomo mutado
+    """
+  
+    idx1, idx2 = random.sample(range(len(chromosome)), 2)
+    chromosome[idx1], chromosome[idx2] = chromosome[idx2], chromosome[idx1]
+    return chromosome
 
-# na população inicial, cada elemento do indivíduo é único
-def gerarPopulacaoInicial(numRainhas: int, tamPopulacao: int) -> List[List[int]]:
-    populacao: List[List[int]] = [random.sample(range(numRainhas), numRainhas) for i in range(tamPopulacao)]
-    return populacao
+def has_repeating_numbers(array):
+    """
+    Verifica se um array tem elementos repetidos
+    Parâmetros:    
+    array -- np.array
+    Retorna:
+    True se existe um elemento repetido
+    False se não
+    """
+    unique_elements, counts = np.unique(array, return_counts=True)
+    return np.any(counts > 1)
 
-# realiza a roleta na lista de disponíveis, nao na fitPopulacao
-def resultadoRoleta(fitPopulacao: List[int], disponiveis: List[int], somaFitness: int) -> int:
-    sorteado:int = random.randint(0, somaFitness)
-    somaAtual: int = 0
-    for i in range(len(disponiveis)):
-        somaAtual += fitPopulacao[disponiveis[i]]
-        if somaAtual >= sorteado:
-            return i
-    return len(disponiveis)-1
+def reproduce(parent_chromosome_1, parent_chromosome_2):
+    """
+    Faz o cruzamento entre dois cromossomos
+    faz o crossover em um ponto do cromossomo
+    caso o cruzamento seja invalido ele é refeito
+    Parâmetros:    
+    chromosome1 :  cromossomo pai
+    chromosome2 :  cromossomo mae
+    Retorna:
+    cromossomos filho    
+    """
+    crossover_point = random.randint(0, len(parent_chromosome_1)-1)
+    offspring_chromosome_1 = np.concatenate((parent_chromosome_1[:crossover_point], parent_chromosome_2[crossover_point:]))
+    offspring_chromosome_2 = np.concatenate((parent_chromosome_2[:crossover_point], parent_chromosome_1[crossover_point:]))
+    while (has_repeating_numbers(offspring_chromosome_2) and has_repeating_numbers(offspring_chromosome_2)): 
+        crossover_point = random.randint(0, len(parent_chromosome_1)-1)
+        offspring_chromosome_1 = np.concatenate((parent_chromosome_1[:crossover_point], parent_chromosome_2[crossover_point:]))
+        offspring_chromosome_2 = np.concatenate((parent_chromosome_2[:crossover_point], parent_chromosome_1[crossover_point:]))
 
-# retorna indices de k individuos para reproducao
-def selecaoRoleta(fitPopulacao: List[int], k: int, tamPopulacao: int) -> List[int]:
-    disponiveis: List[int] = [i for i in range(tamPopulacao)]
-    idxsReproducao: List[int] = []
-    somaFitness: int = 0
-    for fitness in fitPopulacao:
-        somaFitness += fitness
+    return offspring_chromosome_1, offspring_chromosome_2
+
+def probability(chromosome):
+    """
+    Calcula a probabilidade de escolha dos pais
+    quanto mais perto do max fitness maior a probabilidade
+    Parâmetros:    
+    chromosome -- np.array com a posição de cada rainha no tabuleiro
+    Retorna:
+    probabilidade de escolha dos pais
+    """
+    number_of_queens = len(chromosome)
+    max_fitness = (number_of_queens*(number_of_queens-1))/2 
+    return fitness(chromosome) / max_fitness
+
+def roullete_pick(population):
+    """
+    Seleciona um cromossomo da população
+    a escolha é feita com base no fitness de cada cromossomo
+    Parâmetros:
+    population -- lista com cromossomos
+    Retorna:
+    index do cromossomo selecionado
+    """    
+    probabilities = [probability(chromosome) for chromosome in population]
+    # normaliza probabilidades
+    total = sum(probabilities)
+    probabilities = [prob/total for prob in probabilities]
+    idx = np.random.choice(len(population), 1, p=probabilities)
+    return idx[0]
+
+
+   
+def epochs(population, mutation_probability):
+    """
+    Realiza o cruzamento, adiciona na nova população 
+    Parâmetros:
+    population -- lista com cromossomos
+    mutation_probability -- probabilidade de ocorrencia de uma mutação
+    Retorna:
+    nova população
+    """
+    new_population = []
+    for i in range(len(population) // 2):
+        parent_chromosome_1 = population[roullete_pick(population)]
+        parent_chromosome_2 = population[roullete_pick(population)]
+        offspring_chromosome_1, offspring_chromosome_2 = reproduce(parent_chromosome_1,
+                                                                   parent_chromosome_2)
+        if random.random() < mutation_probability:
+            offspring_chromosome_1 = mutate(offspring_chromosome_1)
+        if random.random() < mutation_probability:
+            offspring_chromosome_2 = mutate(offspring_chromosome_2)            
+        new_population.append(offspring_chromosome_1)        
+        new_population.append(offspring_chromosome_2)
+    return new_population        
+
+
+def show_chromosome(chromosome):
+    """
+    Printa informações do cromossomo
+    Parâmetros:
+    chromosome -- np.array com a posição de cada rainha no tabuleiro
+    Retorna:
+    None
+    """
+    print("Cromossomo = {},  Fitness = {}".format(str(chromosome),
+                                                  fitness(chromosome)))
     
-    for i in range(k):
-        idxSorteado: int = resultadoRoleta(fitPopulacao, disponiveis, somaFitness)
-        individuoSorteado = disponiveis[idxSorteado]
-        idxsReproducao.append(individuoSorteado)
-        somaFitness -= fitPopulacao[individuoSorteado]
-        disponiveis[idxSorteado], disponiveis[-1] = disponiveis[-1], disponiveis[idxSorteado]
-        disponiveis.pop()
-
-    return idxsReproducao
-
-# Guarda os índices dos elementos de caminho em indices.
-# Ex:  caminho = {1, 3, 5, 4, 2}
-#     indices[5] contém o índice do elemento 5 => 2
-# Assume que indices já tem espaço suficiente alocado.
-def inicializaListaIndices(individuo: List[int]) -> List[int]:
-    indices: List[int] = [0 for i in range(len(individuo))]
-    for i in range(len(individuo)):
-        indices[individuo[i]] = i
-    return indices 
+def solution(population, max_fitness):
+    """
+    Seleciona o cromossomo com uma solução do n-rainhas
+    Parâmetros:
+    population -- lista com cromossomos
+    max_fitness -- inteiro com o valor maximo do fitness
+    Retorna:
+    Cromossomo com uma solução do n-rainhas
+    """
+    for chromosome in population:
+        if fitness(chromosome) == max_fitness:
+            return chromosome
+    print("Solução não encontrada")
     
-def cycleCrossover(pai1: List[int], pai2: List[int]) -> List[List[int]]:
-    pais = [pai1, pai2]
-    filhos: List[List[int]] = [[], []]
-    indicesPais: List[List[int]] = [[], []]
-    for i in range(2):
-        outro: int = (i+1) % 2
-        filhos[i] = pais[outro]
-        indicesPais[outro] = inicializaListaIndices(pais[outro])
-        index: int = 0
-        while True:
-            filhos[i][index] = pais[i][index]; 
-            index = indicesPais[outro][filhos[i][index]]
-            if index == 0:
-                break
-    return filhos
+def time_test():
+    """
+    Testa o tempo médio de execução para o problema de n-rainhas de 4...10
+    Plota um gráfico com o tempo médio
+    Retorna:
+    None
+    """
+    # Resultado
+    #[0.0011059000001296226, 0.001464750000013737, 1.4886926599999242, 0.9575531100000717, 9.563682670000025, 17.365219170000092, 79.44766261000004, 263.24039344]
+    population_size = 100
+    result = []
+    
+    for number_of_queens in range(4,12):   
+        time_list = []
+        for i in range(10):
+            start = time.perf_counter()
+            numbers_of_generations = 1
+            mutation_probability = 0.1
+            max_fitness = (number_of_queens*(number_of_queens-1))/2     
+            population = [create_chromosome(number_of_queens) for i in range(population_size)]    
+            while not max_fitness in [fitness(chromosome) for chromosome in population]:
+                population = epochs(population, mutation_probability)    
+                fitness_list = [fitness(chromosome) for chromosome in population]
+                numbers_of_generations += 1
+    
+            end = time.perf_counter()
+            time_list.append(end-start)
+            print(time_list)
+        result.append(np.mean(time_list))
+    print(result)
+    
 
-# realiza o cruzamento n/2 vezes, sendo n o número de individuos da populacao,
-# gerando uma nova populacao de n filhos
-def cruzamento(populacao: List[List[int]], idxsReproducao: List[int]) -> List[List[int]]:
-    filhos: List[List[int]] = []
-    for i in range(len(populacao) // 2):
-        idxsPais: List[int] = random.sample(idxsReproducao, 2)
-        resultado = cycleCrossover(populacao[idxsPais[0]], populacao[idxsPais[1]])
-        filhos.append(resultado[0])
-        filhos.append(resultado[1])
+    X = [4, 5, 6, 7, 8, 9, 10, 11]
 
-    return filhos
+    Y = result
+    plt.xticks(range(min(X), max(X)+1))
 
-# a mutação seleciona 2 posicoes do gene para serem trocadas
-def mutacao(populacao: List[List[int]], taxaMutacao: float, numRainhas: int):
-    for i in range(len(populacao)):
-        randFloat: float = random.random()
-        if randFloat <= taxaMutacao:
-            rand: List[int] = random.sample(range(numRainhas), 2)
-            populacao[i][rand[0]], populacao[i][rand[1]] = populacao[i][rand[1]], populacao[i][rand[0]]
-
-def manutencao(populacao: List[List[int]], fitPopulacao: List[int], filhos: List[List[int]]):
-    idxPiorFitness: List[int] = []
-    for filho in filhos:
-        fitnessFilho: int = calculoFitness(filho)
-        if len(idxPiorFitness) < 1:
-            idxPiorFitness = selecionaIdxPiorFitness(fitPopulacao)
-        if fitnessFilho >= fitPopulacao[idxPiorFitness[-1]]:
-            fitPopulacao[idxPiorFitness[-1]] = fitnessFilho
-            populacao[idxPiorFitness[-1]] = filho
-            idxPiorFitness.pop()
-
-def solveGenetico(numRainhas: int, tamPopulacao: int, maxIteracoes: int, taxaMutacao: float) -> Tuple[int, int]:
-    populacao: List[List[int]] = gerarPopulacaoInicial(numRainhas, tamPopulacao)
-    fitPopulacao: List[int] = [calculoFitness(individuo) for individuo in populacao]
-    melhorSolucao: Tuple[List[int], int] = selecionaMelhorSolucao(populacao, fitPopulacao)
-    contIteracoesSemMelhora: int = 0
-    iter: int = 0
-    maxFitness: int = sum(range(numRainhas))
-
-    while iter < maxIteracoes:   
-        idxsReproducao: List[int] = selecaoRoleta(fitPopulacao, tamPopulacao//2, tamPopulacao)
-        filhos: List[List[int]] = cruzamento(populacao, idxsReproducao)
-        mutacao(filhos, taxaMutacao, numRainhas)
-        manutencao(populacao, fitPopulacao, filhos)
-
-        melhorSolucaoAtual: Tuple[List[int], int] = selecionaMelhorSolucao(populacao, fitPopulacao) 
-        if melhorSolucaoAtual[1] > melhorSolucao[1]:
-            contIteracoesSemMelhora = 0
-            melhorSolucao = melhorSolucaoAtual
-        else: 
-            contIteracoesSemMelhora += 1
-        iter += 1
-        # print(f"Geracao {iter} / Melhor Resultado: {melhorSolucao[1]}")
-        if melhorSolucao[1] == maxFitness:
-            break
-
-    # print(f"Solucao: {melhorSolucao[0]}")
-    # print(f"Fitness: {melhorSolucao[1]}")
-    # print(f"Melhor Fitness Possível: {maxFitness}")
-
-    return tuple((melhorSolucao[1], iter))
-
-def main():
-    numRainhas: int = 8
-    tamPopulacao: int = 10
-    maxIteracoes: int = 10000
-    taxaMutacao: float = 0.15
-
-    numExecucoes: int = 100
-    listaGeracoes: List[int] = []
-    listaSolucoes: List[int] = []
-    # random.seed(101)
-
-    for i in range(numExecucoes):
-        print(f"Em execucao: {i+1}")
-        melhorSolucao, geracoes = solveGenetico(numRainhas, tamPopulacao, maxIteracoes, taxaMutacao)
-        listaGeracoes.append(geracoes)
-        listaSolucoes.append(melhorSolucao)
-        print(f"\tSolucao: {melhorSolucao}; Gerações: {geracoes}")
+    plt.plot(X, Y, label = "genético")    
+    plt.title("Tempo de execução - Comparação")    
+    plt.xlabel("Número de damas")
+    plt.ylabel("Tempo (em segundos)")
+    plt.savefig("time_of_execution.png",dpi=400)
 
     
-    print(f"\nDados de {numExecucoes} execuções:")
-    print(f"Gerações - Média: {mean(listaGeracoes):.3f}; Desvio Padrão: {stdev(listaGeracoes):.3f}")
+def show_board(chromosome, number_of_queens):
+    """
+    Printa o tabuleiro com a solução de um cromossomo
+    Parâmetros:
+    chromosome -- np.array com a posição de cada rainha no tabuleiro
+    number_of_queens -- inteiro com a qnt de damas
+    Retorna:
+    None.
+    """
+    board = []
+    
+    for x in range(number_of_queens):
+        board.append(["x"] * number_of_queens)
+        
+    for i in range(number_of_queens):
+        board[chromosome[i]][i]="Q" 
+        
+    for row in board:
+        print (" ".join(row))  
 
+def main():  
+    
+    #time_test()
+    
+    print("---------------------------------------")
+    print("| Selecione a complexidade do problema|")
+    print("| 1 - Complexidade baixa - 6 damas    |")
+    print("| 2 - Complexidade média - 8 damas   |")
+    print("| 3 - Complexidade alta - 10 damas    |")
+    print("---------------------------------------")
+    selection = int(input("Digite sua opção: "))
+    if selection == 1:
+        number_of_queens = 6
+    elif selection == 2:
+        number_of_queens = 8
+    elif selection == 3:
+        number_of_queens = 10
+    else:
+        print("Opção inválida")
+        sys.exit()
+
+    population_size = 100
+    mutation_probability = 0.1
+    max_fitness = (number_of_queens*(number_of_queens-1))/2     
+    population = [create_chromosome(number_of_queens) for i in range(population_size)]    
+    numbers_of_generations = 1
+
+    while not max_fitness in [fitness(chromosome) for chromosome in population]:
+        print("----- Geração {} -----".format(numbers_of_generations))
+        print("")
+        population = epochs(population, mutation_probability)    
+        fitness_list = [fitness(chromosome) for chromosome in population]
+        best_chromossome = fitness_list.index(max(fitness_list))
+        
+        print("Cromossomo com maior fitness maxima atual: ")
+        show_chromosome(population[best_chromossome])
+        print("Fitness média ", np.mean(fitness_list))
+        numbers_of_generations += 1
+        #if numbers_of_generations > 1000:
+        #    break
+    
+    print("Solução encontrada na geração {}".format(numbers_of_generations))
+    possible_solution = solution(population, max_fitness)
+    print("Uma solução possível para o problema de {}-damas é {}".format(number_of_queens,
+                                                                         possible_solution))
+    show_board(possible_solution, number_of_queens)
+    
 if __name__ == "__main__":
     main()

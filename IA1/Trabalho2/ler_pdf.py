08f1b6f65c30b1a1b447e7c8aa7e67b63e72d187
Sersasj <sasjsergioalvarezjunior@gmail.com>
2023-04-03 12:06:31 -0300

    ler pdf

---
+++ IA1/Trabalho2/ler_pdf.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 20:00:14 2023
+
+@author: sergi
+"""
+
+import PyPDF2
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
+import collections
+import re
+
+
+# Le todas a paginas do pdf
+with open('image_processing/Going_deeper_with_convolutions.pdf', 'rb') as pdf_file:
+    # Create a PDF reader object
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+    text = ''
+    for i in range(len(pdf_reader.pages)):
+        pdf_page = pdf_reader.pages[i]
+        text += pdf_page.extract_text()
+
+# Remove pontuação
+#pattern = re.compile('[^a-zA-Z]')
+#text = re.sub(pattern, ' ', text)
+
+# Remove as stopwords do conteúdo
+stop_words = set(stopwords.words('english'))
+tokens = word_tokenize(text.lower())
+filtered_tokens = [token for token in tokens if token not in stop_words]
+
+
+
+word_counts = collections.Counter(filtered_tokens)
+
+most_common_words = word_counts.most_common(10)
+
+# Imprime as 10 palavras mais frequentes
+for word, count in most_common_words:
+    print(f'{word}: {count}')

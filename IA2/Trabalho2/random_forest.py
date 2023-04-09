6dbb5817ab89e9a0e12b11e7a8f477c2190ee98d
gth1ago <gabriel.thiagohs@gmail.com>
2023-04-09 19:15:45 -0300

    limpando arquivos

--- IA2/Trabalho2/random_forest.py
+++ IA2/Trabalho2/random_forest.py
@@ -2,17 +2,14 @@ import numpy as np
 import pandas as pd
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
-from sklearn.model_selection import KFold, StratifiedKFold
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold, StratifiedKFold, train_test_split, GridSearchCV
 from sklearn.svm import SVC, LinearSVC
 from sklearn.preprocessing import normalize
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 
-from sklearn.neighbors import KNeighborsClassifier
 import seaborn as sns
 
 def matrizDeDispersao(variaveis):

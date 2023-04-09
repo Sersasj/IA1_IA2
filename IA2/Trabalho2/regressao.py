89fbc263bb1c33608da08adc768bea0b46fa2ae6
gth1ago <gabriel.thiagohs@gmail.com>
2023-04-09 15:20:30 -0300

    update files

--- IA2/Trabalho2/regressao.py
+++ IA2/Trabalho2/regressao.py
@@ -4,8 +4,11 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import f1_score
-
+from sklearn.metrics import f1_score, make_scorer
+from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC, LinearSVC
+from sklearn.preprocessing import normalize
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 
@@ -21,6 +24,19 @@ def matrizDeDispersao(variaveis):
     plt.show()
 
 # Traformar em binário (dependency)
+
+def cross_val_score_model(model, X, y, n_splits=5):
+    # cv = KFold(n_splits=n_splits, shuffle=True, random_state=42)
+    cv = StratifiedKFold(n_splits=n_splits)
+    scores = []
+    for train_index, test_index in cv.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        score = f1_score(y_test, y_pred, average='macro')
+        scores.append(score)
+    return np.mean(scores), np.std(scores)
 
 
 def transformar_dependente(x):
@@ -48,6 +64,7 @@ def transformar_edjef(x):
         
 if __name__ == '__main__':
     test = pd.read_csv('test.csv')
+
     train = pd.read_csv('train.csv')
     print("Importado arquivos!\n")
 
@@ -111,13 +128,120 @@ if __name__ == '__main__':
     print(train.isnull().sum().sort_values(ascending=False))
     # ====================================
 
-    print("Realizando ANALISES!\n")
     # Analises!
     # EDA - Exploratory Data Analysis
 
     # Histograma
+    # analises()
 
-    # Target
+    # ====================================
+
+    # Campos +importante por enquanto:
+    # Target: Objetivo => indica se a família vive ou não em pobreza extrema.
+    # Dependency: se tem ou nao dependente, pode ser binario
+    # rooms: Número de quartos na residência (familias com pouco pode significa + pobreza)
+    # escolari: Anos de escolaridade da pessoa mais escolarizada no domicílio.
+    # meaneduc: Média de anos de escolaridade das pessoas no domicílio.
+
+    # obs.:
+    # id: indexicacao => nao precisa usa no modelo
+    # rez_esc: Anos de atraso escolar. == muito faltante melhor remover
+    # idhogar: Identificador único do domicílio --- pode ter +1 familia na mesma casa => tamanho_familia
+    # Uso do dep_binario em vez do dependency
+
+    # sem as obvias reduntantes**:
+    variaveis = ['tamanho_familia', 'hogar_adul', 'hogar_mayor', 'hogar_total',
+                 'dep_binario', 'edjefe_binario', 'edjefa_binario', 'meaneduc', 'instlevel1', 'instlevel2',
+                 'instlevel3', 'instlevel4', 'instlevel5', 'instlevel6', 'instlevel7', 'instlevel8',
+                 'instlevel9', 'bedrooms', 'overcrowding', 'tipovivi1', 'tipovivi2', 'tipovivi3',
+                 'tipovivi4', 'tipovivi5', 'computer', 'television', 'mobilephone', 'qmobilephone',
+                 'lugar1', 'lugar2', 'lugar3', 'lugar4', 'lugar5', 'lugar6', 'area1', 'area2', 'age',
+                 'SQBescolari', 'SQBage', 'SQBhogar_total', 'SQBedjefe', 'SQBhogar_nin', 'SQBovercrowding',
+                 'SQBdependency', 'SQBmeaned', 'agesq']
+
+    # Dataframes para uso no modelo
+    # Dataframe X contem apenas as variaveis que desejemos como preditora (features) do modelo
+    # Dataframe y contem a variavel de predicao
+    X = train[variaveis]
+    # Normalizar
+    X = normalize(X, axis = 0, norm = 'max')
+    y = train['Target']
+    y = y.values
+    # ====================================
+    # Divisao de treino e validacao
+    print("Divisao de treino e validacao!\n")
+
+    # seed para manter a aleatoriedade *===* troca pelo random_state
+    # np.random.seed(0)
+
+    # print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    # (6689, 47) (2868, 47) (6689,) (2868,)
+    
+    # X Treino: 6689 exemplos de treino
+    # X Validacao: 2868 exemplos de validacao
+    # y treino: 6689 valor correspondente dos ex. de treino
+    # y Validacao: 2868 valor correspondente dos ex. de validacao
+    # ====================================
+    
+    # ESCOLHA 1 = Regressão Logística por ser mais simples :D
+    #   - modelo linear que usa uma função logística para realizar a classificacao multiclasse
+    #   - LogisticRegression
+
+    # instancia do Modelo com parametros
+    # sem o solver e até 1000 iteracao estava com retorno de aviso que nao foi convergido para um resultado otimo
+    print("Criando Modelo com Regressao Linear!\n")
+
+    # divisao 70/30 por ter um conjunto de dados "grande"
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    parametros = {
+        'C'       : [.001, 0.3, .1],
+        'solver'  : ['newton-cg', 'lbfgs', 'liblinear', 'saga'],
+        'max_iter': [1000, 2000, 5000, 1000]
+    }
+
+    regres = LogisticRegression()
+
+    f1 = make_scorer(f1_score , average='macro')
+
+    clf = GridSearchCV(regres,                # modelo
+                   param_grid = parametros,   # parameters
+                   scoring=f1,                # metric for scoring
+                   cv=3,                      # number of folds
+                   verbose=1)                      
+
+    clf.fit(X_train,y_train)
+
+    print("Tuned Hyperparameters :", clf.best_params_ )
+    print("Accuracy :", clf.best_score_)
+
+    # print('Resuls:', clf.cv_results_)
+
+    # # treinar utilizando tambem  oKFold (reparte em varios bloco)    
+    # mean_score, std_score = cross_val_score_model(modelo, X, y, n_splits=5)
+    # print(f"Mean F1 Score: {mean_score:.2f}, Std: {std_score:.2f}")
+
+    
+    # # FINAL COM O TEST.CSV
+    # of_teste = test[variaveis]
+    # of_teste = normalize(of_teste, axis = 0, norm = 'max')
+
+    # p_final = modelo.predict(of_teste)
+    # sub = pd.Series(p_final, index=test['Id'], name='Target')
+    # sub.shape
+    # sub.to_csv('subRegressao.csv', header=True)
+    # print("\nArquivo subRegressao.csv criado do teste oficial!\n")
+    # # Nota no kaggle com regressao linear: 0.25569
+    # # Nota no kaggle da original: 0.19482
+
+    # # vamos usar a base recomendado de avaliacao: macro F1 score
+    # # Recomendado quando há múltiplas classes desbalanceadas
+    # # calcula a média harmônica do F1 score de cada classe, dando o mesmo peso para todas as classes.
+
+
+
+def analises():
+    print("Realizando ANALISES!\n")
     # Maior parte no grupo 4 = nao vulneraveis
     # sns.countplot(x='Target', data=train)
     plt.hist(train['Target'])
@@ -169,102 +293,3 @@ if __name__ == '__main__':
 
     # ... Testar com outras variaveis p ver se acha mais...
     # ** Um histograma com todas nao rodou, demorou muito ae é melhor fazer separadamente
-
-    # ====================================
-
-    # Campos +importante por enquanto:
-    # Target: Objetivo => indica se a família vive ou não em pobreza extrema.
-    # Dependency: se tem ou nao dependente, pode ser binario
-    # rooms: Número de quartos na residência (familias com pouco pode significa + pobreza)
-    # escolari: Anos de escolaridade da pessoa mais escolarizada no domicílio.
-    # meaneduc: Média de anos de escolaridade das pessoas no domicílio.
-
-    # obs.:
-    # id: indexicacao => nao precisa usa no modelo
-    # rez_esc: Anos de atraso escolar. == muito faltante melhor remover
-    # idhogar: Identificador único do domicílio --- pode ter +1 familia na mesma casa
-    # Uso do dep_binario em vez do dependency
-
-    # sem as obvias reduntantes**:
-    variaveis = ['tamanho_familia', 'hogar_adul', 'hogar_mayor', 'hogar_total',
-                 'dep_binario', 'edjefe_binario', 'edjefa_binario', 'meaneduc', 'instlevel1', 'instlevel2',
-                 'instlevel3', 'instlevel4', 'instlevel5', 'instlevel6', 'instlevel7', 'instlevel8',
-                 'instlevel9', 'bedrooms', 'overcrowding', 'tipovivi1', 'tipovivi2', 'tipovivi3',
-                 'tipovivi4', 'tipovivi5', 'computer', 'television', 'mobilephone', 'qmobilephone',
-                 'lugar1', 'lugar2', 'lugar3', 'lugar4', 'lugar5', 'lugar6', 'area1', 'area2', 'age',
-                 'SQBescolari', 'SQBage', 'SQBhogar_total', 'SQBedjefe', 'SQBhogar_nin', 'SQBovercrowding',
-                 'SQBdependency', 'SQBmeaned', 'agesq']
-
-    # Dataframes para uso no modelo
-    # Dataframe X contem apenas as variaveis que desejemos como preditora (features) do modelo
-    # Dataframe y contem a variavel de predicao
-    X = train[variaveis]
-    y = train['Target']
-
-    # ====================================
-    # Divisao de treino e validacao
-    print("Divisao de treino e validacao!\n")
-
-    # seed para manter a aleatoriedade *===* troca pelo random_state
-    # np.random.seed(0)
-    # divisao 70/30 por ter um conjunto de dados "grande"
-    X_treino, X_valid, y_treino, y_valid = train_test_split(X, y, test_size=0.3, random_state=0)
-
-    # print(X_treino.shape, X_valid.shape, y_treino.shape, y_valid.shape)
-    # (6689, 47) (2868, 47) (6689,) (2868,)
-    
-    # X Treino: 6689 exemplos de treino
-    # X Validacao: 2868 exemplos de validacao
-    # y treino: 6689 valor correspondente dos ex. de treino
-    # y Validacao: 2868 valor correspondente dos ex. de validacao
-    # ====================================
-    
-    # Treinamento e avaliação do modelo
-    #  * Exemplos
-    #    - Árvore de decisão
-    #    - Random Forest
-    #    - Naive Bayes
-    #    - Regressão Logística
-    #    - K-Nearest Neighbors (KNN)
-    #    - Support Vector Machines (SVM)
-    #    - Redes Neurais Artificiais (ANN)
-
-    # ESCOLHA 1 = Regressão Logística por ser mais simples :D
-    #   - modelo linear que usa uma função logística para realizar a classificacao multiclasse
-    #   - LogisticRegression
-
-    # instancia do Modelo com parametros
-    # sem o solver e até 1000 iteracao estava com retorno de aviso que nao foi convergido para um resultado otimo
-    print("Criando Modelo com Regressao Linear!\n")
-    modelo = LogisticRegression(solver='saga', max_iter=5000)
-
-    # treinar o modelo
-    modelo.fit(X_treino, y_treino) ## rever
-
-    # predicao dos dados de validacao utilizando o modelo treinado
-    y_pred = modelo.predict(X_valid)
-
-    # cálculo do f1 score macro
-    print("Gerando F1 score macro da predicao!")
-    f1_macro = f1_score(y_valid, y_pred, average='macro')
-    print('F1 score macro:', f1_macro)
-
-    # resultado da previsão
-    # print(y_pred)
-    # Convergendo só para 4!!
-    
-    # FINAL COM O TEST.CSV
-    of_teste = test[variaveis]
-    p_final = modelo.predict(of_teste)
-    sub = pd.Series(p_final, index=test['Id'], name='Target')
-    sub.shape
-    sub.to_csv('subRegressao.csv', header=True)
-    print("\nArquivo subRegressao.csv criado do teste oficial!\n")
-    # Nota no kaggle com regressao linear: 0.25569
-    # Nota no kaggle da original: 0.19482
-
-    # vamos usar a base recomendado de avaliacao: macro F1 score
-    # Recomendado quando há múltiplas classes desbalanceadas
-    # calcula a média harmônica do F1 score de cada classe, dando o mesmo peso para todas as classes.
-
-

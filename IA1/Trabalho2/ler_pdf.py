cee1691d783c428b0f46606615d8b5864530d01a
gth1ago <gabriel.thiagohs@gmail.com>
2023-04-15 18:58:36 -0300

    update org

--- IA1/Trabalho2/ler_pdf.py
+++ IA1/Trabalho2/ler_pdf.py
@@ -10,50 +10,81 @@ import tabula
 from nltk.stem import PorterStemmer
 import json
 
-
-def concatenate_word(text):
+def concatenate_word():
+    global text
     text_lines = text.splitlines()
-    
+
     for i in range(len(text_lines)):
         if len(text_lines[i]) == 0:
             continue
-        
-        if(text_lines[i][-1] ==  "-"):
+
+        if (text_lines[i][-1] == "-"):
             next_line_words = text_lines[i+1].split(" ")
-            text_lines[i] =  text_lines[i][:-1] + next_line_words[0]
+            text_lines[i] = text_lines[i][:-1] + next_line_words[0]
             text_lines[i+1] = ' '.join(next_line_words[1:])
 
     text = ''
     for i in text_lines:
         text += i
-    return text
-# Le todas a paginas do pdf
-with open('image_processing/Going_deeper_with_convolutions.pdf', 'rb') as pdf_file:
-    # Create a PDF reader object
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
-    text = ''
-    for i in range(len(pdf_reader.pages)):
-        pdf_page = pdf_reader.pages[i]
-        text += pdf_page.extract_text()
-        
-text = concatenate_word(text)
-# Remove pontuação
 
-text = re.sub(r'\n', ' ', text)
-text = re.sub(r'[^a-zA-Z\s]', '', text)
-#print(text)
+# realiza a limpeza dos texto
+def preprocessamento():
+    global text
 
-# Remove as stopwords do conteúdo
-stop_words = set(stopwords.words('english'))
-tokens = word_tokenize(text.lower())
-filtered_tokens = [token for token in tokens if token not in stop_words]
+    concatenate_word()
+    
+    # Remove pontuação
+    text = re.sub(r'\n', ' ', text)
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    # print(text)
 
 
+def identificaTermos():
+    
+    # verifica se possui as stopword
+    try:
+        stopwords.words('english')
+    except:
+        nltk.download('stopwords')
+        nltk.download('punkt')
 
-word_counts = collections.Counter(filtered_tokens)
+    # Remove as stopwords do conteúdo
+    stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(text.lower())
+    filtered_tokens = [token for token in tokens if token not in stop_words]
 
-most_common_words = word_counts.most_common(10)
+    word_counts = collections.Counter(filtered_tokens)
 
-# Imprime as 10 palavras mais frequentes
-for word, count in most_common_words:
-    print(f'{word}: {count}')
+    most_common_words = word_counts.most_common(10)
+
+    # Imprime as 10 palavras mais frequentes
+    for word, count in most_common_words:
+        print(f'{word}: {count}')
+
+
+if __name__ == '__main__':
+
+    # path = 'testes/Teste_2pag_Particle.pdf'
+    # path = 'image_processing/Going_deeper_with_convolutions.pdf'
+    path = './artificial_intelligence/Particle_swarm_optimization.pdf'
+    
+    # Le todas a paginas do pdf
+    with open(path, 'rb') as pdf_file:
+        # Cria obj de leitura do pdf
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        info = pdf_reader.metadata
+        text = ''
+        # for i in range(len(pdf_reader.pages)):
+        for i in range(len(pdf_reader.pages)):
+            pdf_page = pdf_reader.pages[i]
+            text += pdf_page.extract_text()
+            # print(text)
+
+    print('Autor(es):', info.author)
+    print('Titulo:', info.title, '\n')
+    
+    preprocessamento()
+
+    print(pdf_reader.pages[0])
+
+    # identificaTermos()
